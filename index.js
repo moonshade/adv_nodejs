bd1e0408f3e21f88ce2f4dfd8f6033d4cef679db
Jimmy Pan <jimmypan@gmail.com>
2020-06-14 14:50:50 -0700

    Refactor index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+process.env.UV_THREADPOOL_SIZE = 1; // every child in the cluster only has 1 thread available
 const cluster = require('cluster');
 
 // is the file being executed in master node?
@@ -5,26 +6,17 @@ if (cluster.isMaster) {
     // cause index.js to be executed again 
     // but in child mode or slave mode
     cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
 } else {
     // I'm a child and I'm going to act like a server
     // and do nothing else
     const express = require('express');
     const app = express();
-
-    function doWork(duration) {
-        const start = Date.now();
-        while (Date.now() - start < duration) {
-
-        }
-    }
-
+    const crypto = require('crypto');
 
     app.get('/', (req, res) => {
-        doWork(5000);
-        res.send('Hi there');
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hi there');
+        });
     });
 
     app.get('/fast', (req, res) => {
